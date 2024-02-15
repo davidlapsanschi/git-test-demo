0d1b33b1ec1ffb54d53325820cd1a3712d645e50
David Lapsanschi <david.lapsanschi@gmail.com>
2024-02-15 11:06:25 +0200

    Inital commit. Added the my-first-sql file and random-info file.

---
+++ my-first-sql.sql
@@ -0,0 +1,10 @@
+SELECT 
+        fo.id order_id,
+        fo.s_key order_s_key,
+        fo.d_order_status_id,
+        order_created valid_from,
+        NULL valid_to,
+        1 is_current
+    FROM warehouse.f_orders fo 
+        LEFT JOIN warehouse.f_orderstatus_history t_osh ON fo.id = t_osh.order_id
+WHERE t_osh.id is null;
