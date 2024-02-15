b85ec5d5e9116184268c46fc8c43e4597bca2cdf
David Lapsanschi <david.lapsanschi@gmail.com>
2024-02-15 12:21:50 +0200

    fioxed query names

--- my-first-sql.sql
+++ my-first-sql.sql
@@ -1,12 +1,12 @@
 SELECT 
-        fo.id order_idx,
-        fo.name namex,
-        fo.s_key order_s_keyx,
-        fo.d_order_status_idx,
-        order_created valid_fromx,
+        fo.id order_id,
+        fo.name name,
+        fo.s_key order_s_key,
+        fo.d_order_status_id,
+        order_created valid_from,
         NULL valid_to,
         0 is_current
     FROM warehouse.f_orders fo 
         LEFT JOIN warehouse.f_orderstatus_history t_osh ON fo.id = t_osh.order_id
-WHERE t_osh.id is not null;
+WHERE t_osh.id is null;
 
