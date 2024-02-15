13da7016bfa269068e1ef608f60092f48cf48ae3
David Lapsanschi <david.lapsanschi@gmail.com>
2024-02-15 11:35:53 +0200

    EVO-111: Added name and fixed null error

--- my-first-sql.sql
+++ my-first-sql.sql
@@ -1,5 +1,6 @@
 SELECT 
         fo.id order_id,
+        fo.nmae name,
         fo.s_key order_s_key,
         fo.d_order_status_id,
         order_created valid_from,
@@ -7,4 +8,4 @@ SELECT
         1 is_current
     FROM warehouse.f_orders fo 
         LEFT JOIN warehouse.f_orderstatus_history t_osh ON fo.id = t_osh.order_id
-WHERE t_osh.id is null;
+WHERE t_osh.id is not null;
