ff2a3a33b65176475facbc32de2412045cdd5247
Iulia Barlea <iulia.barlea98@yahoo.com>
2024-02-15 12:25:49 +0200

    Updated query

--- my-first-sql.sql
+++ my-first-sql.sql
@@ -1,12 +1,12 @@
 SELECT 
-        fo.id order_idx,
-        fo.name namex,
-        fo.s_key order_s_keyx,
-        fo.d_order_status_idx,
-        order_created valid_fromx,
+        fo.id order_ids,
+        fo.name names,
+        fo.s_key order_s_keys,
+        fo.d_order_status_ids,
+        order_created valid_froms,
         NULL valid_to,
         0 is_current
     FROM warehouse.f_orders fo 
         LEFT JOIN warehouse.f_orderstatus_history t_osh ON fo.id = t_osh.order_id
 WHERE t_osh.id is not null;
-
+SELECT * from orders;
